3fc5b50b2387c27e70207a736bde8e660a4724ce
Vasidovi <vasidovi@gmail.com>
2019-09-17 17:39:16 +0300

    in authentication service added auto log out when jwt token expires and snackbar warning when expiration time is in 5 min

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,20 @@
 import { UserService } from './user.service';
 import { IUser } from './../models/user.model';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient,
-              private userService: UserService,
+  constructor(private userService: UserService,
+              private _snackBar: MatSnackBar,
               private cookie: CookieService) {}
+
+
+sessionWarningCalled = false;
 
 login(user: IUser): Promise<any> {
 
@@ -19,6 +22,10 @@ login(user: IUser): Promise<any> {
     this.userService.signin(user).then(data => {
       this.cookie.set('role', JSON.parse(atob(data.token.split('.')[1])).scopes);
       this.cookie.set('token', data.token);
+
+      // expiration time in seconds
+      this.cookie.set('exp', JSON.parse(atob(data.token.split('.')[1])).exp);
+      this.sessionWarningCalled = false;
       resolved();
     }, rejected);
   });
@@ -36,7 +43,26 @@ return this.cookie.get('role') === 'ROLE_ADMIN';
 
 isLoggedIn(): boolean {
   if (this.cookie.get('token')){
-    return true;
+    const warnTimeInSeconds = 300;
+    const currentTimeInSeconds = new Date().getTime() / 1000;
+    if ( +this.cookie.get('exp') > currentTimeInSeconds + warnTimeInSeconds){
+      return true;
+
+    } else if (+this.cookie.get('exp') > currentTimeInSeconds){
+      if (!this.sessionWarningCalled){
+      this._snackBar.open( 'Your session will end in ' +  Math.round(+this.cookie.get('exp') - currentTimeInSeconds) + ' seconds ', 'X', {
+        duration: 2000,
+      });
+      this.sessionWarningCalled = true;
+    }
+      return true;
+    } else {
+      this._snackBar.open( 'Your session has ended logging out' , 'X', {
+        duration: 2000,
+      });
+      this.logout();
+      return false;
+    }
   } else {
     return false;
     }
