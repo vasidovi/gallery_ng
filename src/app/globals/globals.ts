b05956639e90ccbd54187b0bdb945c8cb36fcbd2
Vasidovi <vasidovi@gmail.com>
2019-09-21 00:04:48 +0300

    added postinstall script for heroku app

--- src/app/globals/globals.ts
+++ src/app/globals/globals.ts
@@ -1,4 +1,7 @@
 'use strict';
 
-export const hostName = 'http://localhost:8080';
-export const domainName = 'localhost:8080';
+// export const hostName = 'http://localhost:8080';
+// export const domainName = 'localhost:8080';
+
+export const hostName = 'https://gallery-in.herokuapp.com';
+export const domainName = 'gallery-in.herokuapp.com';
