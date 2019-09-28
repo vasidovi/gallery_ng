433abd8c698b8d87fdf1b05e85f9062731f08088
Vasidovi <vasidovi@gmail.com>
2019-09-28 18:43:31 +0300

    removed unused imports from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
 import { JwtModule } from '@auth0/angular-jwt';
 import { environment } from './../environments/environment';
 
-import {
-  GalleryService,
-  AuthService,
-  UserService
-} from './services';
-
 import { MaterialModule } from './material.module';
 import { SrcPipeModule } from './srcPipe.module';
-import { FormComponentsModule } from './formComponets.module';
-import { DirectivesModule } from './directives.module';
 
 import { AppComponent } from './app.component';
 
@@ -40,16 +30,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     FooterComponent,
   ],
   imports: [
-    DirectivesModule,
-    FormComponentsModule,
     SrcPipeModule,
     MaterialModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    FormsModule,
     NgbModule,
     JwtModule.forRoot({
       config: {
@@ -62,7 +48,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
       }
     })
   ],
-  providers: [GalleryService, AuthService, UserService, CookieService],
+  providers: [CookieService],
   bootstrap: [AppComponent],
   entryComponents: [PhotoDialogComponent]
 })
