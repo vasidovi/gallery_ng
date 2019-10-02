b49bf1ecbeed2f3d57cde0b227d02b1280fcea67
Vasidovi <vasidovi@gmail.com>
2019-10-02 21:17:47 +0300

    According to angular loadChildren() should be in format loadChildren: () => import('IMPORT_STRING').then(M => M.EXPORT_NAME)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./views/gallery/gallery.module').then(mod => mod.GalleryModule) },
-  { path: 'upload',  loadChildren: () => import('./views/image-upload/image-upload.module').then(mod => {
-    return mod.ImageUploadModule;
-  }), canActivate: [AuthGuard] },
-  { path: 'image/edit/:id', loadChildren: () => import('./views/image-edit/image-edit.module').then(mod => {
-    return mod.ImageEditModule;
-  }), canActivate: [AuthGuard] },
+  { path: 'upload',  loadChildren: () => import('./views/image-upload/image-upload.module')
+    .then(mod => mod.ImageUploadModule), canActivate: [AuthGuard] },
+  { path: 'image/edit/:id', loadChildren: () => import('./views/image-edit/image-edit.module')
+    .then(mod =>  mod.ImageEditModule), canActivate: [AuthGuard] },
   { path: 'signup', loadChildren: () => import('./views/sign-up/sign-up.module').then(mod => mod.SignUpModule) },
   { path: 'signin', loadChildren: () => import('./views/sign-in/sign-in.module').then(mod => mod.SignInModule) },
 ];
